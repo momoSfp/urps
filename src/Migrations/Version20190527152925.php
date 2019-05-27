a3954674488998babdde5e2c65a0c715c1e55588
mahdjoub <mmahdjoub@santeformapro.com>
2019-05-27 17:30:00 +0200

    migration

--- src/Migrations/Version20190527152925.php
+++ src/Migrations/Version20190527152925.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190521123116 extends AbstractMigration
+final class Version20190527152925 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,7 +22,8 @@ final class Version20190521123116 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE image ADD image_name VARCHAR(255) NOT NULL, ADD image_size INT NOT NULL, ADD updated_at DATETIME NOT NULL, DROP url, DROP caption');
+        $this->addSql('ALTER TABLE tutor CHANGE postcode postcode VARCHAR(5) DEFAULT NULL');
+        $this->addSql('ALTER TABLE image CHANGE image_size image_size INT NOT NULL, CHANGE updated_at updated_at DATETIME NOT NULL');
     }
 
     public function down(Schema $schema) : void
@@ -30,6 +31,7 @@ final class Version20190521123116 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE image ADD caption VARCHAR(255) NOT NULL COLLATE utf8mb4_unicode_ci, DROP image_size, DROP updated_at, CHANGE image_name url VARCHAR(255) NOT NULL COLLATE utf8mb4_unicode_ci');
+        $this->addSql('ALTER TABLE image CHANGE image_size image_size INT DEFAULT NULL, CHANGE updated_at updated_at DATETIME DEFAULT NULL');
+        $this->addSql('ALTER TABLE tutor CHANGE postcode postcode VARCHAR(30) DEFAULT NULL COLLATE utf8mb4_unicode_ci');
     }
 }
