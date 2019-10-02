2e7a366fab08c5396fa1574a7b92b905a91be197
mahdjoub <mmahdjoub@santeformapro.com>
2019-10-02 17:41:13 +0200

    correction nom formulaire et acces public

--- src/Controller/PublicController.php
+++ src/Controller/PublicController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Content;
 use App\Repository\ContentRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -26,5 +27,27 @@ class PublicController extends AbstractController
         [
             'contents' => $contents,
         ]);
-    } 
+    }
+
+    /**
+     * Play game
+     * 
+     * @Route("/{slug}/play", name="public_play")
+     * 
+     * @return Response
+     */
+    public function play(Content $content)
+    {
+        $a = 0;
+        $b = $_POST["cid"]; if (!$b) $b = 0;
+        $c = $_POST["www"]; if (!$c) $c = "~";
+        $d = $_POST["api"]; if (!$d) $d = "~";
+        $SFP_DATA = implode("__", array($a, $b, $c, $d));
+
+        return $this->render('content/play.html.twig', 
+        [
+            'content' => $content,
+            'SFP_DATA' => $SFP_DATA
+        ]);
+    }    
 }
